f7f5c205277360f9ef53e7bee3b58d7354a898b6
Sathish <jobad.database@gmail.com>
2026-09-15 18:28:06 +0530

    Update locations.js

--- locations.js
+++ locations.js
@@ -1,6 +1,6 @@
 // locations.js
 var LOCATIONS = [
 
-    { pin_id: 50000403, pincode: "500004", name: "Khairatabad", district: "HYDERABAD", state: "TG", lat: 17.40710242, lng: 78.46248076 }
+    
     { pin_id: 50011201, pincode: "500112", name: "Badangpet", district: "HYDERABAD", state: "TG", lat: 17.30505, lng: 78.525178 }
 ];
