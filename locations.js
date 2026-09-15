52ca4256dc184cdae93396faf148aed79411f278
Sathish <jobad.database@gmail.com>
2026-09-15 18:27:22 +0530

    Update locations.js

--- locations.js
+++ locations.js
@@ -1,20 +1,6 @@
 // locations.js
 var LOCATIONS = [
-    { pin_id: 30540201, pincode: "305402", name: "Bahera", district: "Mulugu", state: "TG", lat: 26.240127, lng: 74.915001 },
-    { pin_id: 31160103, pincode: "311601", name: "Shopura (Gega Ka Khera)", district: "Mulugu", state: "TG", lat: 25.289691, lng: 74.947233 },
-    { pin_id: 33202803, pincode: "332028", name: "Kirdoli Bari", district: "Mulugu", state: "TG", lat: 27.531834, lng: 74.748933 },
-    { pin_id: 33302206, pincode: "333022", name: "Bhojgarh", district: "Mulugu", state: "TG", lat: 27.786485, lng: 75.557723 },
-    { pin_id: 33551310, pincode: "335513", name: "9 Ssw", district: "Mulugu", state: "TG", lat: 29.523681, lng: 74.379204 },
-    { pin_id: 33552610, pincode: "335526", name: "13 Ksp-B", district: "Mulugu", state: "TG", lat: 29.5564167, lng: 74.5040278 },
-    { pin_id: 34302701, pincode: "343027", name: "Arwa", district: "Mulugu", state: "TG", lat: 24.889028, lng: 71.383665 },
-    { pin_id: 34302704, pincode: "343027", name: "Varanwa", district: "Mulugu", state: "TG", lat: 24.773477, lng: 71.483059 },
-    { pin_id: 50000101, pincode: "500001", name: "Hyderabad G.P.O.", district: "HYDERABAD", state: "TG", lat: 17.387993, lng: 78.47652 },
-    { pin_id: 50000102, pincode: "500001", name: "Moazzampura", district: "HYDERABAD", state: "TG", lat: 17.65563889, lng: 78.75202778 },
-    { pin_id: 50000103, pincode: "500001", name: "Putlibowli", district: "HYDERABAD", state: "TG", lat: 17.2306, lng: 78.29121 },
-    { pin_id: 50000201, pincode: "500002", name: "Charminar", district: "HYDERABAD", state: "TG", lat: 17.368663, lng: 78.477662 },
-    { pin_id: 50000301, pincode: "500003", name: "Secunderabad", district: "HYDERABAD", state: "TG", lat: 17.44128087, lng: 78.4952238 },
-    { pin_id: 50000302, pincode: "500003", name: "Rail Nilayam", district: "HYDERABAD", state: "TG", lat: 17.44128087, lng: 78.4952238 },
-    { pin_id: 50000401, pincode: "500004", name: "A.Gs Office", district: "HYDERABAD", state: "TG", lat: 17.40655556, lng: 78.46702778 },
-    { pin_id: 50000402, pincode: "500004", name: "Parishram Bhawan", district: "HYDERABAD", state: "TG", lat: 17.40158333, lng: 78.47502778 },
+
     { pin_id: 50000403, pincode: "500004", name: "Khairatabad", district: "HYDERABAD", state: "TG", lat: 17.40710242, lng: 78.46248076 }
+    { pin_id: 50011201, pincode: "500112", name: "Badangpet", district: "HYDERABAD", state: "TG", lat: 17.30505, lng: 78.525178 }
 ];
