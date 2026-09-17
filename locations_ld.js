6b4f1ecdc1808d67183a909663434b94d3c9b62e
Sathish <jobad.database@gmail.com>
2026-09-17 15:14:22 +0530

    Create locations_ld.js

---
+++ locations_ld.js
@@ -0,0 +1,13 @@
+// locations_ld.js
+var LOCATIONS = [
+    { pin_id: 68255101, pincode: "682551", name: "Androth", district: "LAKSHADWEEP DISTRICT", state: "LD", lat: 10.815331, lng: 73.681487 },
+    { pin_id: 68255201, pincode: "682552", name: "Amini", district: "LAKSHADWEEP DISTRICT", state: "LD", lat: 11.124792, lng: 72.724956 },
+    { pin_id: 68255301, pincode: "682553", name: "Agathi", district: "LAKSHADWEEP DISTRICT", state: "LD", lat: 10.864102, lng: 72.198152 },
+    { pin_id: 68255401, pincode: "682554", name: "Bithra", district: "LAKSHADWEEP DISTRICT", state: "LD", lat: 11.598982, lng: 72.186846 },
+    { pin_id: 68255402, pincode: "682554", name: "Chetlat", district: "LAKSHADWEEP DISTRICT", state: "LD", lat: 11.691404, lng: 72.711116 },
+    { pin_id: 68255501, pincode: "682555", name: "Kavaratti", district: "LAKSHADWEEP DISTRICT", state: "LD", lat: 10.5667, lng: 72.6417 },
+    { pin_id: 68255601, pincode: "682556", name: "Kadamat", district: "LAKSHADWEEP DISTRICT", state: "LD", lat: 11.227927, lng: 72.778605 },
+    { pin_id: 68255701, pincode: "682557", name: "Kalpeni", district: "LAKSHADWEEP DISTRICT", state: "LD", lat: 10.067286, lng: 73.64425 },
+    { pin_id: 68255801, pincode: "682558", name: "Kiltan", district: "LAKSHADWEEP DISTRICT", state: "LD", lat: 11.486575, lng: 73.0065 },
+    { pin_id: 68255901, pincode: "682559", name: "Minicoy", district: "LAKSHADWEEP DISTRICT", state: "LD", lat: 8.274767, lng: 73.049009 },
+];
