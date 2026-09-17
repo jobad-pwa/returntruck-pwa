ce8e2f01104a3fdf3f1e75313070175aa4cd1166
Sathish <jobad.database@gmail.com>
2026-09-17 15:15:28 +0530

    Update locations_ld.js

--- locations_ld.js
+++ locations_ld.js
@@ -9,5 +9,5 @@ var LOCATIONS = [
     { pin_id: 68255601, pincode: "682556", name: "Kadamat", district: "LAKSHADWEEP DISTRICT", state: "LD", lat: 11.227927, lng: 72.778605 },
     { pin_id: 68255701, pincode: "682557", name: "Kalpeni", district: "LAKSHADWEEP DISTRICT", state: "LD", lat: 10.067286, lng: 73.64425 },
     { pin_id: 68255801, pincode: "682558", name: "Kiltan", district: "LAKSHADWEEP DISTRICT", state: "LD", lat: 11.486575, lng: 73.0065 },
-    { pin_id: 68255901, pincode: "682559", name: "Minicoy", district: "LAKSHADWEEP DISTRICT", state: "LD", lat: 8.274767, lng: 73.049009 },
+    { pin_id: 68255901, pincode: "682559", name: "Minicoy", district: "LAKSHADWEEP DISTRICT", state: "LD", lat: 8.274767, lng: 73.049009 }
 ];
